0eff4ddc8fca264bb5e35647ef50d55240e12681
Kevin Olbrich <kevin.olbrich@gmail.com>
2009-12-05 11:43:26 -0500

    Set a default for the dev.host and live.host configs so specs behave consistently, and then fix the site_preview_url to maintain the request port number regardless of the config settings.

--- app/helpers/admin/preview_helper.rb
+++ app/helpers/admin/preview_helper.rb
@@ -10,13 +10,15 @@ module Admin::PreviewHelper
   def site_preview_url(mode, page)
     page = mode == :dev ? page.current_dev : page.current_live
     protocol = (@controller || self).request.protocol
-    host = (@controller || self).request.host_with_port
+    host = (@controller || self).request.host
     host = case mode
     when :dev
       Radiant::Config['dev.host'] || ("dev." + host)
     when :live
       Radiant::Config['live.host'] || host
     end
-    protocol + host + page.url
+    port = (@controller || self).request.port
+    port = ([80, 443].include?(port.to_i)) ? "" : ":#{port}"
+    protocol + host + port + page.url
   end
 end
